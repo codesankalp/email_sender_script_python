ac902b6b43393131509a9bdc177bbf17a1959708
sankalp <sankalp123427@gmail.com>
2020-07-27 15:38:36 +0530

    removing py cache

--- send_email.py
+++ send_email.py
@@ -1,13 +1,9 @@
 from email_sender import generate_email, send_message
-
 sender = input('Enter your mail : ')
 reciever = input('Enter reciepent mail : ')
-
 subject = input('Enter Subject: ')
 body = input('Enter mail body : ')
 attachment_path = input("Attachment path (leave blank if no attachment) : ")
 if attachment_path == '': attachment_path = None
-
 message = generate_email(sender,reciever,subject,body,attachment_path)
-
 send_message(message)
